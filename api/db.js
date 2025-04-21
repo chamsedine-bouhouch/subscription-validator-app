495887a9c1f6d861682af62c3d01d89d930c3e3c
chamsedine-bouhouch <bouhouchchamseddine@gmail.com>
2025-04-21 22:28:14 +0200

    update environment configuration, modify database URL, and enhance frontend HTML structure

--- api/db.js
+++ api/db.js
@@ -5,10 +5,10 @@ dotenv.config();
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
+    // ssl: {
+    //   require: true,
+    //   rejectUnauthorized: false,
+    // },
   },
   logging: console.log, // Logs every SQL query
   pool: {
